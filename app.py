89b8d18592964a8dd57d16bad0626d6eab4f9e00
dlelo <akinyidonattah@gmail.com>
2017-11-28 13:26:25 +0300

    update and delete shopping list items

--- app.py
+++ app.py
@@ -197,12 +197,11 @@ def delete_shoppinglist(id):
 
 LISTITEMS = []
 
-@app.route('/shoppinglistitems', methods = ['POST','GET'])
-def shoppinglistitems():
+@app.route('/shoppinglistitems/<int:id>/', methods = ['POST','GET'])
+def shoppinglistitems(id):
     if request.method == 'POST':
         if 'uname' in session and 'list_name' in session:
             dict_of_list_items = {}
-            
 
             itemname = request.form.get('item')
             itemquantity = request.form.get('quantity')
@@ -216,6 +215,11 @@ def shoppinglistitems():
             LISTITEMS.append(dict_of_list_items)
             print(LISTITEMS)
 
+            #create sessions for the items
+            session['item_name'] = itemname
+            session['quantity'] = itemquantity
+            session['price'] = itemprice
+
 
             message = MY_SHOPPING_LIST.add_shopping_list_item(itemname, itemquantity, itemprice)
             if message == "shopping list item successfully added":
@@ -225,7 +229,93 @@ def shoppinglistitems():
             return "Login to proceed"
 
 
-    return render_template('shoppinglistitems.html', items=LISTITEMS)
+    return render_template('shoppinglistitems.html', id=id, items=LISTITEMS)
+
+#single shoppinglist item
+@app.route('/update_shoppinglistitems/<int:id>/', methods=['POST', 'GET'])
+def update_shoppinglistitems(id):
+    """function that updates user shopping lists items"""
+    old_dict_of_list_items = {}
+
+    old_itemname = session['item_name']
+    old_quantity = session['quantity']
+    old_price = session['price']
+
+    old_dict_of_list_items['item_name'] = old_itemname
+    old_dict_of_list_items['quantity'] = old_quantity
+    old_dict_of_list_items['price'] = old_price
+
+    print (old_dict_of_list_items)
+
+    if request.method == 'POST':
+        dict_of_list_items = {}
+        if 'uname' in session:
+            if dict_of_list_items not in LISTITEMS:
+                new_itemname = request.form.get('item')
+                new_quantity = request.form.get('quantity')
+                new_price = request.form.get('price')
+
+                #adding items to dictionary
+                dict_of_list_items['item_name'] = new_itemname
+                dict_of_list_items['quantity'] = new_quantity
+                dict_of_list_items['price'] = new_price
+                
+                
+                session['item_name'] = new_itemname
+                session['quantity'] = new_quantity
+                session['price'] = new_price
+
+                LISTITEMS.remove(old_dict_of_list_items)
+                LISTITEMS.append(dict_of_list_items)
+
+                print (LISTITEMS)
+
+                flash("You have successfully updated your shopping list items")
+                return redirect(url_for('shoppinglistitems', id=id))
+            else:
+                flash("There is already a shopping listitem with that name")
+                return redirect(url_for('shoppinglistitems', id=id))
+        else:
+            flash("Login to proceed")
+            return redirect(url_for('login'))
+
+
+    return render_template('update_shoppinglistitems.html', id=id, itemname=old_itemname, itemquantity=old_quantity, itemprice=old_price)
+
+# delete shopping list items
+@app.route('/delete_shoppinglistitems/<int:id>/', methods=['POST', 'GET'])
+def delete_shoppinglistitems(id):
+    """function that deletes user shopping lists items"""
+    old_dict_of_list_items = {}
+
+    old_itemname = session['item_name']
+    old_quantity = session['quantity']
+    old_price = session['price']
+
+    old_dict_of_list_items['item_name'] = old_itemname
+    old_dict_of_list_items['quantity'] = old_quantity
+    old_dict_of_list_items['price'] = old_price
+
+    print (old_dict_of_list_items)
+
+    if request.method == 'POST':
+        if 'uname' in session:
+            if old_dict_of_list_items in LISTITEMS:
+
+                LISTITEMS.remove(old_dict_of_list_items)
+                print (LISTITEMS)
+
+                flash("You have successfully deleted the shopping list item")
+                return redirect(url_for('shoppinglistitems', id=id))
+            else:
+                flash("The shopping listitem has not been deleted")
+                return redirect(url_for('shoppinglistitems', id=id))
+        else:
+            flash("Login to proceed")
+            return redirect(url_for('login'))
+
+
+    return render_template('delete_shoppinglistitems.html', id=id, itemname=old_itemname, itemquantity=old_quantity,itemprice=old_price )
 
 @app.route('/getsession')
 def getsession():
